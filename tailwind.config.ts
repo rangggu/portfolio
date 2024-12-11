2a7ed477d99f07a9d090ead1ae09feb61737571c
aliyah521 <aliyah521@bitgarma.biz>
2024-12-11 15:26:46 +0900

    Init : 초기 환경 설정
    - 소스코드 폴더 구조 정립
    - 초기 라이브러리 설치
    - config 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
@@ -8,11 +8,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors: {},
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
