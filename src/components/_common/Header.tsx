2dbccf2b990954d551e0cae01847b7f3470fa439
rangggu <aliyah521@naver.com>
2024-12-26 14:47:17 +0900

    Style : 헤더에 배경색 추가

--- src/components/_common/Header.tsx
+++ src/components/_common/Header.tsx
@@ -12,8 +12,10 @@ export default memo(function Header(props: HeaderProps) {
   return (
     <header
       className={cn(
-        "fixed top-0 left-0 flex items-center justify-center gap-20 w-full h-20 px-10 text-caption1 duration-200 transition-all",
-        tab === TAB.MAIN ? "opacity-0 pointer-events-none bg-transparent" : "opacity-100",
+        "fixed top-0 left-0 flex items-center justify-center gap-20 w-full h-20 px-10 text-caption1 duration-200 transition-all z-10",
+        tab === TAB.MAIN
+          ? "opacity-0 pointer-events-none bg-transparent"
+          : "opacity-100 bg-blue700",
       )}
     >
       <span>PROLOGUE</span>
