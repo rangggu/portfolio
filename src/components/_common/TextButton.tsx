053edcea3ee9fc78f6d40d2900fcb38dc2417e47
rangggu <aliyah521@naver.com>
2024-12-27 16:23:39 +0900

    Refactor : 텍스트 버튼 파라미터에 위치 속성 추가

--- src/components/_common/TextButton.tsx
+++ src/components/_common/TextButton.tsx
@@ -9,17 +9,27 @@ interface Props {
   delay: number
   isVisible: boolean
   onClick: MouseEventHandler<HTMLButtonElement>
+  left?: number | string
+  right?: number | string
+  top?: number | string
+  bottom?: number | string
 }
 
 export default function TextButton(props: Props) {
-  const { text, delay, isVisible = true, onClick } = props
+  const { text, delay, isVisible = true, onClick, left, right, top, bottom } = props
   const buttonAnimation = useAnimation(isVisible, buttonVariants(delay))
 
   return (
     <motion.button
       {...buttonAnimation}
-      className="absolute bottom-3 right-3 flex items-center text-caption1 font-light text-gray300"
+      className="absolute flex items-center text-caption1 font-light text-gray300"
       onClick={onClick}
+      style={{
+        left: left,
+        right: right,
+        top: top,
+        bottom: bottom,
+      }}
     >
       {text}
       <MdKeyboardArrowRight className="w-5 h-5" />
