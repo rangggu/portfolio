a872e6ceeb236d50edb15865799ef92f613b472c
rangggu <aliyah521@naver.com>
2025-01-06 22:20:22 +0900

    Style : career 페이지에 프로젝트 컴포넌트 높이 수정 및 내용 변경

--- src/components/career/Description/Project/Item/index.tsx
+++ src/components/career/Description/Project/Item/index.tsx
@@ -16,12 +16,12 @@ export default function Item({ title, subtitle, desc, project, setProject }: Pro
   return (
     <motion.div
       className={cn(
-        "flex flex-col gap-4 w-full h-20 px-5 py-5 rounded-lg bg-black bg-opacity-15 font-light cursor-default transition-all duration-500 overflow-hidden",
-        active ? "h-auto max-h-[203px] bg-opacity-25" : "h-auto max-h-[67px]",
+        "flex flex-col gap-4 w-full h-20 px-5 py-7 rounded-lg bg-black bg-opacity-15 font-light cursor-default transition-all duration-500 overflow-hidden",
+        active ? "h-auto max-h-[219px] bg-opacity-25" : "h-auto max-h-[83px]",
       )}
       onMouseOver={() => setProject(title)}
     >
-      <ul className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-2">
         <p>{title}</p>
         <p className={cn("text-gray300", active ? "opacity-100" : "opacity-0 delay-300")}>
           {subtitle}
--- src/components/career/Description/Project/index.tsx
+++ src/components/career/Description/Project/index.tsx
@@ -20,34 +20,25 @@ export default function Project() {
     },
     {
       title: "인체 균형 능력 분석 서비스",
-      subtitle: "인체 균형 능력을 측정하여 개인 맞춤형 솔루션을 제공하는 모바일 앱",
+      subtitle: "인체 균형 능력측정 및 맞춤 솔루션을 제공하는 모바일 앱",
       desc: [
-        "각종 데이터 분석 차트 및 PDF 출력 기능 개발",
-        "주기적/지속적인 소통으로 고객 요구사항을 업데이트에 반영",
-        "프로그램 시연에 참여하여 사용자 유입을 촉진, 재이용률 40% 증가",
-      ],
-    },
-    {
-      title: "발형상 균형 분석 서비스",
-      subtitle: "발형상 분석을 통해 사용자의 균형 상태를 실시간으로 제공하는 데스크톱 앱",
-      desc: [
-        "터치 스크린이 탑재된 키오스크에 최적화된 Electron 앱 구현",
-        "발형상 감지 센서 프로세스와 실시간 연동 수행",
-        "Heatmap 시각화 설계로 고객사 만족도 85% 이상 달성",
+        "연령 모드에 따른 서비스 화면 개선",
+        "측정 장치와의 연동 기능 구현",
+        "각종 데이터 차트 및 PDF 출력 기능 개발",
       ],
     },
     {
       title: "기업/행사 정보 안내 서비스",
-      subtitle: "로컬 환경에 최적화되어 다양한 정보를 표시/제공하는 데스크톱 앱",
+      subtitle: "다양한 정보를 표시/제공하는 키오스크 전용 데스크톱 앱",
       desc: [
-        "터치 스크린이 탑재된 키오스크에 최적화된 Electron 앱 구현",
+        "Electron을 통한 데스크톱 앱 구현",
         "오프라인 데이터 관리를 가능하게 하여 유지보수 편의성을 향상, 고객사 만족도 85% 이상 달성",
       ],
     },
   ]
 
   return (
-    <div className="flex flex-col justify-between gap-2.5 w-[48%]">
+    <div className="flex flex-col justify-between gap-3 w-[48%]">
       <div className="relative flex items-center w-full">
         <h3 className="text-gray100 font-semibold">참여 프로젝트</h3>
         <MoreButton
@@ -57,7 +48,7 @@ export default function Project() {
           }}
         />
       </div>
-      <div className="flex flex-col gap-1.5 w-full h-[422px] text-body2">
+      <div className="flex flex-col gap-3 w-full h-[422px] text-body2">
         {PROJECT.map((item, index) => (
           <Item {...item} key={index} project={project} setProject={setProject} />
         ))}
